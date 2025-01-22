81b241069117cf70ce6bdda603b2297722d30732
Kenyetta Griffin (She/Her) <96461060+PursuitMadeMe@users.noreply.github.com>
2025-01-21 21:07:36 -0500

    LoadGeminiKey Frontend Middleware JSON format

--- app.js
+++ app.js
@@ -2,7 +2,19 @@
 
 const express = require ('express');
 
+const cors = require ('cors');
+
+const dotenv = require ('dotenv');
+
 const app = express();
+
+dotenv.config();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
 
 //Define our ROUTES : Health Check with GET / method = GET path = /
 app.get('/', (request,response) => {
