63cab4c1846027a4c3079e264ce2127dcd360750
Kenyetta Griffin (She/Her) <96461060+PursuitMadeMe@users.noreply.github.com>
2025-01-21 20:59:03 -0500

    GeminiAPIKey

--- server.js
+++ server.js
@@ -2,6 +2,15 @@
 
 require('dotenv').config();
 
+const geminiApiKey = process.env.GEMINI_API_KEY;
+
+console.log(geminiApiKey)
+
+if (geminiApiKey) {
+  } else {
+    console.error('GEMINI_API_KEY not found!');
+  }
+
 const app = require ('./app');
 
 const PORT = process.env.PORT || 9000
