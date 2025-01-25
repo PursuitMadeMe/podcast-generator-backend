2006e8bee3c24a40d0db827412575a5cd0da7127
Kenyetta Griffin (She/Her) <96461060+PursuitMadeMe@users.noreply.github.com>
2025-01-25 17:16:15 -0500

    /uploads Path static

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 //App.js : define the route handlers
 
 const express = require ('express');
-
 const cors = require ('cors');
-
 const dotenv = require ('dotenv');
-
-const app = express();
 
 dotenv.config();
 
+const app = express();
 app.use(cors());
-
+app.use('/uploads', express.static('uploads'));
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
 //Define our ROUTES : Health Check with GET / method = GET path = /
